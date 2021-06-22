6a8d212576937b77c611d9df646f79c057a129ff
Andrei Teleron <ateleron2@gmail.com>
2021-06-22 14:13:03 -0500

    Updated the staticfile_dirs

--- wagtail_react_ecommerce_tutorial/settings/base.py
+++ wagtail_react_ecommerce_tutorial/settings/base.py
@@ -175,7 +175,7 @@ STATICFILES_FINDERS = [
 
 STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, 'static'),
-    os.path.join(BASE_DIR, '/frontend/build/static'),
+    os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
 
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
