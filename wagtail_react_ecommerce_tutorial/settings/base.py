d31bc0423b49994259dcd19d9061ce9c2dba7304
Andrei Teleron <ateleron2@gmail.com>
2021-06-22 13:35:09 -0500

    updated STATICFILES_DIRS

--- wagtail_react_ecommerce_tutorial/settings/base.py
+++ wagtail_react_ecommerce_tutorial/settings/base.py
@@ -175,8 +175,10 @@ STATICFILES_FINDERS = [
 
 STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, 'static'),
-    os.path.join(BASE_DIR, 'frontend/build/static'),
+    os.path.join(BASE_DIR, 'frontend\\build\\static'),
 ]
+
+print('STATIFCILES_DIRS = ', STATICFILES_DIRS)
 
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
 # JavaScript / CSS assets being served from cache (e.g. after a Wagtail upgrade).
