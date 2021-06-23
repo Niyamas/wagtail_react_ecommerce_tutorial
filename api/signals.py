af53f049294e76cfd4c35a927cb70905fe1282b0
Andrei Teleron <ateleron2@gmail.com>
2021-06-23 13:50:57 -0500

    Removed migrations

--- api/signals.py
+++ api/signals.py
@@ -4,15 +4,15 @@ from django.contrib.auth.models import User
 
 
 # Listen for when the User updates.
-#@receiver(pre_save, sender=User)
-#def update_user(instance, **kwargs):
-"""
-update_user is called before a User object is saved/updated.
-"""
+@receiver(pre_save, sender=User)
+def update_user(instance, **kwargs):
+    """
+    update_user is called before a User object is saved/updated.
+    """
     #print('Signal Triggered')
 
-#    user = instance
+    user = instance
 
     # When a user object is saved/updated and the user's email is not empty, make the username the same as the user's email.
-#    if user.email != '':
-#        user.username = user.email
+    if user.email != '':
+        user.username = user.email
