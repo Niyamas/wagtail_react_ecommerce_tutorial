49e0408ec6ea781b547cb069fb0e03ebb373d5c0
Andrei Teleron <ateleron2@gmail.com>
2021-06-24 16:23:20 -0500

    Updated ordering of latest products

--- api/views.py
+++ api/views.py
@@ -90,7 +90,7 @@ class ItemListView(ListAPIView):
         # Get the keyword URL parameter value. Return empty string if the keyword is empty.
         # Filter the queryset based on the value of keyword and the queryset object's title.
         keyword = self.request.query_params.get('keyword', '')
-        queryset = self.queryset.filter(title__icontains=keyword)
+        queryset = self.queryset.filter(title__icontains=keyword).order_by('-first_published_at')
 
         if isinstance(queryset, QuerySet):
             # Ensure queryset is re-evaluated on each request.
